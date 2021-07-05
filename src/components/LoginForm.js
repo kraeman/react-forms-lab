05ce93f6b913da82a643393b7c9e461018c266ff
Krae Wind <kraewind@gmail.com>
2021-07-05 17:58:18 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,52 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      UN: null,
+      PW: null
+    };
+    this.handlePWChange = this.handlePWChange.bind(this)
+    this.handleUNChange = this.handleUNChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-    this.state = {};
+
+
+
+  handleUNChange = (event) => {
+    this.setState({UN: event.target.value})
+  }
+
+  handlePWChange = (event) => {
+    this.setState({PW: event.target.value})
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if(this.state.UN && this.state.PW) {
+      let formData = { UN: this.state.UN, PW: this.state.PW }
+      this.props.handleLogin(formData)
+    }else{ 
+      null
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={event => this.handleUNChange(event)}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={event => this.handlePWChange(event)}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {charsRemaining:this.props.maxChars};
+    this.handleCharChange = this.handleCharChange.bind(this);
+  }
 
-    this.state = {};
+
+  handleCharChange = (event) => {
+      let chars = event.target.value.length
+      this.setState({charsRemaining: 280 - chars})
+
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.handleCharChange(event)} />
+        <h5>{this.state.charsRemaining}</h5>
       </div>
     );
   }
