d235852f7329e4b790db651de5f5cb9a8ae81e7f
Krae Wind <kraewind@gmail.com>
2021-07-07 19:38:58 -0400

    commit tree 31fcd12702faf7bc87c3c85c77ee890f5ce83cdf

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -5,7 +5,7 @@ class TwitterMessage extends React.Component {
     super(props);
     this.state = {
                   charsRemaining:this.props.maxChars,
-                  input:null
+                  input: ""
     };
     this.handleCharChange = this.handleCharChange.bind(this);
   }
@@ -14,7 +14,7 @@ class TwitterMessage extends React.Component {
   handleCharChange = (event) => {
       let chars = event.target.value.length
       this.setState({charsRemaining: 280 - chars,
-                     input:event.target.value
+                     input: event.target.value
       })
 
   }
@@ -23,7 +23,7 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" onChange={event => this.handleCharChange(event)} />
+        <input type="text" name="message" id="message" value={this.state.input} onChange={event => this.handleCharChange(event)} />
         <h5>{this.state.charsRemaining}</h5>
       </div>
     );
